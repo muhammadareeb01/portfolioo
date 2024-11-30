cd017948edea38b203ccf357a3525f655efed8fc
muhammadareeb01 <muhammadareeb795@gmail.com>
2024-11-30 15:32:13 +0500

    donee

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -2,21 +2,17 @@ export const skillsData = [
   'HTML',
   'CSS',
   'Javascript',
-  'Typescript',
+ 
   'React',
   'Next JS',
   'Tailwind',
   'MongoDB',
-  'MySQL',
-  'PostgreSQL',
+
   'Git',
-  'AWS',
+  
   'Bootstrap',
-  'Docker',
-  'Go',
-  'Figma',
+ 
   'Firebase',
   'MaterialUI',
-  'Nginx',
-  'Strapi'
+  
 ]
