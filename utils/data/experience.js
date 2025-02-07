d0213388ce221c33f705273af9324e5c86e7d2ea
talha7k <talha7k@gmail.com>
2025-02-08 00:13:23 +0500

    updated

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,26 @@
 export const experiences = [
   {
-      id: 1,
-      title: 'FrontEnd Developer',
-      company: "Karlsruher Institut für Technologie (KIT)",
-      duration: "(Aug 2024 - Present)"
+    id: 1,
+    title: "React Developer",
+    company: "SoftDips",
+    duration: "(Nov 2024 - Jan 2025)",
   },
   {
-      id: 2,
-      title: "Level 2 Freelancer Frontend Developer",
-      company: "Fiverr",
-      duration: "(Remote)"
+    id: 2,
+    title: "FrontEnd Developer",
+    company: "Karlsruher Institut für Technologie (KIT)",
+    duration: "(Aug 2024 - Present)",
   },
   {
-      id: 3,
-      title: "FrontEnd Developer Intern",
-      company: "MONITR (NIC STARTUP)",
-      duration: "(Jul 2023 - Aug 2023)"
-  }
+    id: 3,
+    title: "Level 2 Freelancer Frontend Developer",
+    company: "Fiverr",
+    duration: "(Remote)",
+  },
+  {
+    id: 3,
+    title: "FrontEnd Developer Intern",
+    company: "MONITR (NIC STARTUP)",
+    duration: "(Jul 2023 - Aug 2023)",
+  },
 ];
